c7322d10afb8ed0160a8201231cbde6d1eb02bf0
Konstantinos K <raistlindragonl@gmail.com>
2024-10-23 11:31:59 +0300

    Add Infinite scrolling

--- src/components/PokemonList.tsx
+++ src/components/PokemonList.tsx
@@ -1,8 +1,10 @@
+import React from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
 import usePokemon from "../hooks/usePokemon";
 import PokemonCard from "./PokemonCard";
 
 const PokemonList = () => {
-  const { data: pokemon, isLoading, error } = usePokemon();
+  const { data, isLoading, error, hasNextPage, fetchNextPage } = usePokemon();
 
   if (isLoading) {
     return <div>Loading...</div>;
@@ -12,12 +14,25 @@ const PokemonList = () => {
     return <div>Error: {error.message}</div>;
   }
 
+  const fetchedPokemonCount =
+    data?.pages.reduce((total, page) => total + page.results.length, 0) || 0;
+
   return (
-    <div className="pokemon-list">
-      {pokemon?.results.map((pokemon, index) => (
-        <PokemonCard key={index} pokemon={pokemon} />
+    <InfiniteScroll
+      className="pokemon-list"
+      hasMore={!!hasNextPage}
+      dataLength={fetchedPokemonCount}
+      next={fetchNextPage}
+      loader={<div>Loading...</div>}
+    >
+      {data?.pages?.map((page, index) => (
+        <React.Fragment key={index}>
+          {page.results.map((pokemon) => (
+            <PokemonCard key={pokemon.name} pokemon={pokemon} />
+          ))}
+        </React.Fragment>
       ))}
-    </div>
+    </InfiniteScroll>
   );
 };
 
--- src/hooks/usePokemon.ts
+++ src/hooks/usePokemon.ts
@@ -1,13 +1,17 @@
-import { useQuery } from "react-query";
+import { useInfiniteQuery } from "react-query";
 import ApiClient, { FetchResponse } from "../services/apiClient";
 import { Pokemon } from "../types";
 
 const apiClient = new ApiClient<Pokemon>("/pokemon");
 
 const usePokemon = () => {
-  return useQuery<FetchResponse<Pokemon>, Error>({
+  return useInfiniteQuery<FetchResponse<Pokemon>, Error>({
     queryKey: ["pokemon"],
-    queryFn: () => apiClient.getAll(),
+    queryFn: ({ pageParam = 1 }) => {
+      return apiClient.getAll({ params: { offset: (pageParam - 1) * 20 } });
+    },
+    getNextPageParam: (lastPage, allPages) =>
+      lastPage.next ? allPages.length + 1 : undefined,
     staleTime: 24 * 60 * 60 * 1000, // 24 hours
   });
 };
