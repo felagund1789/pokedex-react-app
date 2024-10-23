a0726a0a27e672617c9abb44e027041a1da89f5f
Konstantinos K <raistlindragonl@gmail.com>
2024-10-23 12:16:53 +0300

    Replace react-query with @tanstack/react-query

--- src/hooks/usePokemon.ts
+++ src/hooks/usePokemon.ts
@@ -1,4 +1,4 @@
-import { useInfiniteQuery } from "react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import ApiClient, { FetchResponse } from "../services/apiClient";
 import { Pokemon } from "../types";
 
@@ -7,11 +7,14 @@ const apiClient = new ApiClient<Pokemon>("/pokemon");
 const usePokemon = () => {
   return useInfiniteQuery<FetchResponse<Pokemon>, Error>({
     queryKey: ["pokemon"],
-    queryFn: ({ pageParam = 1 }) => {
-      return apiClient.getAll({ params: { offset: (pageParam - 1) * 20 } });
+    queryFn: ({ pageParam }) => {
+      return apiClient.getAll({
+        params: { offset: (Number(pageParam) - 1) * 20 },
+      });
     },
     getNextPageParam: (lastPage, allPages) =>
       lastPage.next ? allPages.length + 1 : undefined,
+    initialPageParam: 1,
     staleTime: 24 * 60 * 60 * 1000, // 24 hours
   });
 };
