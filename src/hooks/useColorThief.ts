a5329756f79a0c3b3fb5e9a3263c8dc24484c1df
felagund1789 <raistlindragonl@gmail.com>
2024-10-24 12:17:19 +0300

    Update implementation of useColorThief() hook

--- src/hooks/useColorThief.ts
+++ src/hooks/useColorThief.ts
@@ -3,10 +3,12 @@ import { useEffect, useState } from "react";
 
 const colorthief = new ColorThief();
 
-const rgbToHex = (r: number, g: number, b: number, alpha: number) => {
+const rgbToHex = (r: number, g: number, b: number, alpha: number): string => {
   const toHex = (n: number) => n.toString(16).padStart(2, "0");
   return `#${toHex(r)}${toHex(g)}${toHex(b)}${alpha !== 1 ? toHex(Math.round(alpha * 255)) : ""}`;
 }
+
+const colorBrightness = ([r, g, b]: number[]): number => 0.299 * r + 0.587 * g + 0.114 * b;
 
 const useColorThief = (imgUrl: string) => {
   const [color, setColor] = useState<string | null>(null);
@@ -18,10 +20,14 @@ const useColorThief = (imgUrl: string) => {
     img.style.display = "none";
     img.src = imgUrl;
     img.addEventListener("load", () => {
-      const c = colorthief.getColor(img);
       const palette = colorthief.getPalette(img);
-      setColor(c ? rgbToHex(...c, 1) : null);
-      setPalette(palette ? palette.map(([r, g, b]) => rgbToHex(r, g, b, 1)) : []);
+      setPalette(palette 
+        ? palette
+          .filter(p => colorBrightness(p) > 50)
+          .filter(p => colorBrightness(p) < 200)
+          .map(([r, g, b]) => rgbToHex(r, g, b, 1)) 
+        : []);
+      setColor(palette && palette.length > 0 ? rgbToHex(...palette[0], 1) : null);
       img.remove();
     });
   }, [imgUrl]);
