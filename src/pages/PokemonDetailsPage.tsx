365d62c045bba365415decb6b9884ee9d73fe55a
Konstantinos K <raistlindragonl@gmail.com>
2024-10-30 13:30:34 +0200

    Add buttons to navigate between pages

--- src/pages/PokemonDetailsPage.tsx
+++ src/pages/PokemonDetailsPage.tsx
@@ -1,10 +1,12 @@
 import { useEffect } from "react";
-import { Outlet, useParams } from "react-router-dom";
+import { NavLink, Outlet, useParams } from "react-router-dom";
 import PokemonInfoCard from "../components/pokemonInfoCard/PokemonInfoCard";
 import "../App.css";
+import usePokemonColor from "../hooks/usePokemonColor";
 
 function PokemonDetailsPage() {
   const { name } = useParams();
+  const color = usePokemonColor({ slug: name! });
 
   useEffect(() => {
     window.scrollTo(0, 0);
@@ -17,7 +19,18 @@ function PokemonDetailsPage() {
   return (
     <div className="pokemon-details">
       <PokemonInfoCard slug={name} />
-      <div className="pokemon-details-outlet">
+      <div
+        className="pokemon-details-buttons"
+        style={{ backgroundColor: `${color}` }}
+      >
+        <NavLink to={`/pokemon/${name}/stats`}>Stats</NavLink>
+        <NavLink to={`/pokemon/${name}/forms`}>Forms</NavLink>
+        <NavLink to={`/pokemon/${name}/moves`}>Moves</NavLink>
+      </div>
+      <div
+        className="pokemon-details-outlet"
+        style={{ backgroundColor: `${color}` }}
+      >
         <Outlet />
       </div>
     </div>
