5bf616b35af046d500dcc709a23282f06a5c331f
felagund1789 <raistlindragonl@gmail.com>
2024-11-01 15:34:10 +0200

    Update hook useResourceName()

--- src/hooks/useResourceName.ts
+++ src/hooks/useResourceName.ts
@@ -3,6 +3,9 @@ import { APIResource } from "../types";
 
 const useResourceName = (resource?: APIResource) => {
   const language = "en";
+  const capitalize = (name: string) => name .split("-")
+      .map((w: string) => `${w.charAt(0).toUpperCase()}${w.substring(1)}`)
+      .join(" ");
 
   return useQuery<string, Error>({
     queryKey: [resource?.url, language],
@@ -10,10 +13,11 @@ const useResourceName = (resource?: APIResource) => {
       fetch(resource?.url ?? "")
         .then((res) => res.json())
         .then((data) =>
+          data.names && data.names.length > 0 ?
           data.names.find(
             (name: { name: string; language: APIResource }) =>
               name.language.name === language
-          ).name
+          ).name : capitalize(data.name)
         ),
     staleTime: 24 * 60 * 60 * 1000, // 24 hours
   });
