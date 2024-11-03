36b0026036a691300485944c5b939dde841b7fc8
Konstantinos K <raistlindragonl@gmail.com>
2024-11-03 23:39:47 +0200

    Use localForage to persist query results
    - Add PersistQueryClient and PersistQueryClientProvider
      in order to asynchronously persist query results

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,40 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { createAsyncStoragePersister } from "@tanstack/query-async-storage-persister";
+import { QueryClient } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import {
+  persistQueryClient,
+  PersistQueryClientProvider,
+} from "@tanstack/react-query-persist-client";
+import localForage from "localforage";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes";
 
-const queryClient = new QueryClient();
+const persister = createAsyncStoragePersister({
+  storage: localForage,
+  key: "react-query-cache",
+});
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      gcTime: 24 * 60 * 60 * 1000, // 24 hours
+      staleTime: 24 * 60 * 60 * 1000, // 24 hours
+    },
+  },
+});
+
+persistQueryClient({ queryClient, persister });
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
+    <PersistQueryClientProvider
+      client={queryClient}
+      persistOptions={{ persister }}
+    >
       <RouterProvider router={router} />
       <ReactQueryDevtools />
-    </QueryClientProvider>
+    </PersistQueryClientProvider>
   </StrictMode>
 );
