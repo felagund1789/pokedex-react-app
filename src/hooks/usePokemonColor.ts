77a9184a32175ce68d7ee2491f2a45a5bfafe7ad
felagund1789 <raistlindragonl@gmail.com>
2024-11-04 10:26:06 +0200

    Make usePokemonColor() load image only if needed

--- src/hooks/usePokemonColor.ts
+++ src/hooks/usePokemonColor.ts
@@ -1,31 +1,57 @@
+import ColorThief from "colorthief";
 import { useEffect, useState } from "react";
-import useColorThief from "./useColorThief";
 import usePokemon from "./usePokemon";
+
+const colorthief = new ColorThief();
+const artworkBaseURL = import.meta.env.VITE_POKEMON_ARTWORK_BASE_URL;
+
+const rgbToHex = (r: number, g: number, b: number, alpha: number) => {
+  const toHex = (n: number) => n.toString(16).padStart(2, "0");
+  return `#${toHex(r)}${toHex(g)}${toHex(b)}${
+    alpha !== 1 ? toHex(Math.round(alpha * 255)) : ""
+  }`;
+};
+
+const colorBrightness = ([r, g, b]: number[]): number =>
+  0.299 * r + 0.587 * g + 0.114 * b;
 
 interface Props {
   slug: string;
 }
-
-const artworkBaseURL = import.meta.env.VITE_POKEMON_ARTWORK_BASE_URL;
 
 const usePokemonColor = ({ slug }: Props) => {
   const { data } = usePokemon({ slug });
   const [pokemonColor, setPokemonColor] = useState<string | null>(null);
   const imgUrl = data?.id ? `${artworkBaseURL}${data.id}.png` : "";
 
-  const { color } = useColorThief(imgUrl);
-
   useEffect(() => {
-    const storedColor = localStorage.getItem(slug);
+    const storedColor = localStorage.getItem(imgUrl);
     if (storedColor) {
       setPokemonColor(storedColor);
-    }
-  }, [slug]);
+    } else {
+      const img = document.createElement("img");
+      img.crossOrigin = "anonymous";
+      img.style.display = "none";
+      img.src = imgUrl;
+      img.addEventListener("load", () => {
+        let dominantColor = colorthief.getColor(img);
+        const palette = colorthief
+          .getPalette(img)
+          .filter((c) => colorBrightness(c) > 63 && colorBrightness(c) < 224);
 
-  if (color) {
-    localStorage.setItem(slug, color);
-    return color;
-  }
+        dominantColor =
+          colorBrightness(dominantColor) > 63 &&
+          colorBrightness(dominantColor) < 224
+            ? dominantColor
+            : palette[0];
+
+        const color = rgbToHex(...dominantColor, 1);
+        localStorage.setItem(imgUrl, color);
+        setPokemonColor(color);
+        img.remove();
+      });
+    }
+  }, [imgUrl]);
 
   return pokemonColor;
 };
