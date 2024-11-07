b003a3cd2dc110a2bf111130b0800cfb53eacab2
Konstantinos K <raistlindragonl@gmail.com>
2024-11-07 22:43:37 +0200

    Use image caching

--- src/services/pokedexService.ts
+++ src/services/pokedexService.ts
@@ -1,6 +1,10 @@
 import { Pokedex } from "pokeapi-js-wrapper";
 
 const timeout = 24 * 60 * 60 * 1000;
-const pokedex = new Pokedex({ cache: true, timeout });
+const pokedex = new Pokedex({
+  cache: true,
+  cacheImages: true,
+  timeout,
+});
 
 export default pokedex;
