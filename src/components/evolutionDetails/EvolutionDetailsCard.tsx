d7717b42ebb49fc747b66d0d96af687f8f0e47c2
Konstantinos K <raistlindragonl@gmail.com>
2026-09-04 13:38:44 +0300

    style: update color for pokemon moves and stat bar components

--- src/components/evolutionDetails/EvolutionDetailsCard.tsx
+++ src/components/evolutionDetails/EvolutionDetailsCard.tsx
@@ -23,7 +23,7 @@ const EvolutionDetailsCard = ({ details }: Props) => {
         height="24px"
         viewBox="0 -960 960 960"
         width="24px"
-        fill="white"
+        fill="black"
         className="arrow"
       >
         <path d="M647-440H160v-80h487L423-744l57-56 320 320-320 320-57-56 224-224Z" />
